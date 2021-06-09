b5b26901f3524e5912fc832558f0964c937968c6
dmart411 <dmart411@fiu.edu>
2021-06-09 10:47:58 -0400

    searchbar api request

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,30 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { useState } from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import youtube from "../api/youtube";
+
 function App() {
-  return <div>App</div>;
+  const [videos, setVideos] = useState([]);
+
+  const onFormSubmit = (term) => {
+    youtube
+      .get("/search", {
+        params: {
+          q: term,
+        },
+      })
+      .then((res) => setVideos(res.data.items))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <Container>
+      <SearchBar onFormSubmit={onFormSubmit} />
+      <VideoList videos={videos} />
+    </Container>
+  );
 }
 
 export default App;
