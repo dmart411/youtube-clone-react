d899e6b78095c44d01281c556db91477a4c083ec
dmart411 <dmart411@fiu.edu>
2021-06-09 12:47:42 -0400

    added video details component and styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import { Container } from "react-bootstrap";
 import { useState } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDeatails from "./VideoDetails";
 import youtube from "../api/youtube";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
   const onFormSubmit = (term) => {
     youtube
@@ -15,14 +17,31 @@ function App() {
           q: term,
         },
       })
-      .then((res) => setVideos(res.data.items))
+      .then((res) => {
+        setVideos(res.data.items);
+        setSelectedVideo(res.data.items[0]);
+      })
       .catch((err) => console.log(err));
+  };
+
+  const onSelectVideo = (video) => {
+    setSelectedVideo(video);
+    console.log(selectedVideo);
   };
 
   return (
     <Container>
       <SearchBar onFormSubmit={onFormSubmit} />
-      <VideoList videos={videos} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDeatails video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videos} onSelectVideo={onSelectVideo} />
+          </div>
+        </div>
+      </div>
     </Container>
   );
 }
